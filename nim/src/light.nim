ed6fa2b2e369e43868b68c239a7ef0c069d99fa2
Enrique <niofis@gmail.com>
2017-08-17 16:58:55 -0700

    attempt to add a separate view class for windows management

--- nim/src/light.nim
+++ nim/src/light.nim
@@ -1,129 +1,32 @@
 import  lightpkg/renderer,
         lightpkg/job,
         lightpkg/image,
-        sdl2/sdl,
-        sdl2/sdl_gfx_primitives as gfx,
-        times,
-        strutils,
+        lightpkg/view,
         sequtils
-
 
 
 const
   Title = "Light"
   ScreenW = 800
   ScreenH = ((ScreenW / 16) * 9).int
-  WindowFlags = 0
-  RendererFlags = sdl.RendererAccelerated
 
-type
-  App = ref AppObj
-  AppObj = object
-    window*: sdl.Window
-    renderer*: sdl.Renderer
-
-proc init(app: App): bool =
-  if sdl.init(sdl.InitVideo) != 0:
-    sdl.logCritical(sdl.LogCategoryError,
-                    "Can't initialize SDL: %s",
-                    sdl.getError())
-    return false
-
-  app.window = sdl.createWindow(
-    Title,
-    sdl.WindowPosUndefined,
-    sdl.WindowPosUndefined,
-    ScreenW,
-    ScreenH,
-    WindowFlags)
-  if app.window == nil:
-    sdl.logCritical(sdl.LogCategoryError,
-                    "Can't create window: %s",
-                    sdl.getError())
-    return false
-
-  app.renderer = sdl.createRenderer(app.window, -1, RendererFlags)
-  if app.renderer == nil:
-    sdl.logCritical(sdl.LogCategoryError,
-                    "Can't create renderer: %s",
-                    sdl.getError())
-    return false
-
-  return true;
-
-proc exit(app: App) =
-  app.renderer.destroyRenderer()
-  app.window.destroyWindow()
-  sdl.quit()
-
-proc events(pressed: var seq[sdl.Keycode]): bool =
-  result = false
-  var e: sdl.Event
-  if pressed != nil:
-    pressed = @[]
-
-  while sdl.pollEvent(addr(e)) != 0:
-    if e.kind == sdl.Quit:
-      return true
-    elif e.kind == sdl.KeyDown:
-      if pressed != nil:
-        pressed.add(e.key.keysym.sym)
-      if e.key.keysym.sym == sdl.K_Escape:
-        return true
-
+proc update() =
+  #do the rendering
+  #let res = render(jb, RayTracing)
+  #let res = render(jb, NullTracing)
+  #for p in 0..<ScreenH * ScreenW:
+  #  pixels[p] = res[p].toARGB()
+  echo "updating view"
 
 proc main() =
-  var
-    app = App(window: nil, renderer: nil)
-    done = false
-    pressed: seq[sdl.Keycode] = @[]
-    pixels: array[ScreenW * ScreenH, uint32]
+  var view = newView(Title, ScreenW, ScreenH, update)
 
-  if init(app) == false:
+  if view.init() == false:
     return
 
-  let jb = newJob(resolution = (ScreenW, ScreenH))
+  view.start()
 
-  var texture = app.renderer.createTexture(
-              sdl.PIXEL_FORMAT_ARGB8888,
-              sdl.TEXTUREACCESS_STREAMING,
-              ScreenW,
-              ScreenH)
-
-  if texture == nil:
-    sdl.logCritical(sdl.LogCategoryError,
-                    "Can't create texture: %s",
-                    sdl.getError())
-    return
-
-  while not done:
-    let start = epochTime()
-
-    #do the rendering
-    #let res = render(jb, RayTracing)
-    let res = render(jb, NullTracing)
-
-    for p in 0..<ScreenH * ScreenW:
-          pixels[p] = res[p].toARGB()
-
-    #for y in 0..<ScreenH:
-    #  for x in 0..<ScreenW:
-    #    pixels[y * ScreenW + x] = res[y][x].toARGB()
-
-    discard texture.updateTexture(nil, addr(pixels), ScreenW * sizeof(uint32))
-    discard app.renderer.renderCopy(texture, nil, nil)
-    let fps = 1.0/(epochTime() - start)
-    discard app.renderer.stringRGBA(0,
-                0,
-                (fps.formatFloat).cstring,
-                255.uint8,
-                255.uint8,
-                255.uint8,
-                255.uint8)
-    app.renderer.renderPresent()
-    done = events(pressed)
-
-  exit(app)
+  #let jb = newJob(resolution = (ScreenW, ScreenH))
 
 main()
 
---
+++ nim/src/lightpkg/view.nim
@@ -0,0 +1,122 @@
+import  sdl2/sdl,
+        sdl2/sdl_gfx_primitives as gfx,
+        times,
+        strutils,
+        sequtils
+
+const
+  WindowFlags = 0
+  RendererFlags = sdl.RendererAccelerated
+
+type
+  View* = ref ViewObj
+  ViewObj = object
+    window*: sdl.Window
+    renderer*: sdl.Renderer
+    title*: string
+    width*: int
+    height*: int
+    update*: proc()
+    pixels*: seq[uint32]
+
+proc newView*(title:string, width:int, height:int, update: proc()): View =
+  var view = View()
+  view.title = title
+  view.width = width
+  view.height = height
+  view.update = update
+  view.window = nil
+  view.renderer = nil
+  view.pixels = newSeq[uint32](width * height)
+  return view
+
+proc init*(view: View): bool =
+  if sdl.init(sdl.InitVideo) != 0:
+    sdl.logCritical(sdl.LogCategoryError,
+                    "Can't initialize SDL: %s",
+                    sdl.getError())
+    return false
+
+  view.window = sdl.createWindow(
+    view.title,
+    sdl.WindowPosUndefined,
+    sdl.WindowPosUndefined,
+    view.width,
+    view.height,
+    WindowFlags)
+  if view.window == nil:
+    sdl.logCritical(sdl.LogCategoryError,
+                    "Can't create window: %s",
+                    sdl.getError())
+    return false
+
+  view.renderer = sdl.createRenderer(view.window, -1, RendererFlags)
+  if view.renderer == nil:
+    sdl.logCritical(sdl.LogCategoryError,
+                    "Can't create renderer: %s",
+                    sdl.getError())
+    return false
+
+  return true;
+
+proc exit(view: View) =
+  view.renderer.destroyRenderer()
+  view.window.destroyWindow()
+  sdl.quit()
+
+proc events(pressed: var seq[sdl.Keycode]): bool =
+  result = false
+  var e: sdl.Event
+  if pressed != nil:
+    pressed = @[]
+
+  while sdl.pollEvent(addr(e)) != 0:
+    if e.kind == sdl.Quit:
+      return true
+    elif e.kind == sdl.KeyDown:
+      if pressed != nil:
+        pressed.add(e.key.keysym.sym)
+      if e.key.keysym.sym == sdl.K_Escape:
+        return true
+
+
+proc start*(view: View) =
+  var
+    done = false
+    pressed: seq[sdl.Keycode] = @[]
+
+  if init(view) == false:
+    return
+
+  var texture = view.renderer.createTexture(
+              sdl.PIXEL_FORMAT_ARGB8888,
+              sdl.TEXTUREACCESS_STREAMING,
+              view.width,
+              view.height)
+
+  if texture == nil:
+    sdl.logCritical(sdl.LogCategoryError,
+                    "Can't create texture: %s",
+                    sdl.getError())
+    return
+
+  while not done:
+    let start = epochTime()
+
+    view.update()
+    echo view.pixels.len
+
+    discard texture.updateTexture(nil, addr(view.pixels), view.width * sizeof(uint32))
+    discard view.renderer.renderCopy(texture, nil, nil)
+    let fps = 1.0/(epochTime() - start)
+    discard view.renderer.stringRGBA(0,
+                0,
+                (fps.formatFloat).cstring,
+                255.uint8,
+                255.uint8,
+                255.uint8,
+                255.uint8)
+    view.renderer.renderPresent()
+    done = events(pressed)
+
+  exit(view)
