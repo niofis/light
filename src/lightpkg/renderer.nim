c6b160680983e3eff97e22836cf855072f58031d
Enrique <niofis@gmail.com>
2017-12-07 21:16:07 -0700

    small refactor

--- src/lightpkg/renderer.nim
+++ src/lightpkg/renderer.nim
@@ -4,67 +4,17 @@ import  strutils,
         pmap,
         sequtils,
         pmap,
-        threadpool
+        threadpool,
+        pathtracing,
+        raytracing
 
 import  vector, ray, camera, color, material, sphere, ray, hit, world, job, solid
 export  vector, ray, camera, color, material, sphere, ray, hit, world, job, solid
 
-const MAXDEPTH = 5
-
 type RenderMethod* = enum
   RayTracing, PathTracing, NullTracing
 
-randomize()
-
-proc rnd2(): float32 = (2.0 * random(1.0)) - 1.0
-
-proc rndDome(normal: Vector3): Vector3 =
-  var d:float32
-  var p:Vector3
-
-  d = -1.0
-
-  while d < 0:
-    p = ((rnd2(), rnd2(), rnd2())).unit
-    d = p.dot(normal)
-  return p
-
-proc pathTrace(w: World, r: Ray, depth: int): Color =
-  var did_hit = false
-  var hit = nohit
-  var out_color = color.Black
-  var sp:Solid
-
-  for s in w.objects:
-    let lh = s.hit(r)
-    
-    if lh.distance < hit.distance:
-      sp = s
-      did_hit = true
-      out_color = s.material.color
-      hit = lh
-  
-  if did_hit == true and depth < MAXDEPTH:
-    if sp.isLight == false:
-      let nray = (origin: hit.point, direction: rnd_dome(hit.normal))
-      let ncolor = pathTrace(w, nray, depth + 1)
-      let at = nray.direction.dot(hit.normal)
-      out_color = out_color * (ncolor * at)
-
-  if did_hit == false or depth >= MAXDEPTH:
-    out_color = color.Black
-
-  return out_color
-
-proc rayTrace(w: World, r: Ray, depth: int): Color =
-  let hit =
-    w.objects
-      .mapIt((obj: it, ht: it.hit(r)))
-      .filterIt(it.ht != nohit)
-      .foldl(if a.ht.distance < b.ht.distance: a else: b)
-  return hit.obj.material.color
-
-proc nullTrace(w:World, r:Ray, depth: int): Color = color.Black
+proc nullTrace*(w:World, r:Ray, depth: int): Color = color.Black
 
 proc getPrimaryRays(camera: Camera): seq[Ray] =
   var rays = newSeq[Ray]();
