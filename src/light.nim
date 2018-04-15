a0a64fe8e41edcce31cb81f0f838279d66e8adb9
Enrique <niofis@gmail.com>
2018-04-15 02:05:49 -0700

    removed render limit

--- src/light.nim
+++ src/light.nim
@@ -10,13 +10,9 @@ const
   ScreenW = 800
   ScreenH = ((ScreenW / 16) * 9).int
 
-var times = 5
-  
 proc update(job: Job): auto =
   var imgBuffer = newImage(ScreenW, ScreenH)
   result = proc (pixels: var seq[uint32]) =
-    if times == 0:
-      return
     #do the rendering
     #let res = render(job, PathTracing)
     let res = render(job, RayTracing)
@@ -25,8 +21,6 @@ proc update(job: Job): auto =
     
     for p in 0..<ScreenH * ScreenW:
       pixels[p] = (imgBuffer.pixels[p] / imgBuffer.count.float32).toARGB()
-
-    times = times - 1
 
 proc main() =
   let jb = newJob(resolution = (ScreenW, ScreenH))
