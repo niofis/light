ead5cef3d2456892f86cbab89be94690ac6190f0
Enrique <niofis@gmail.com>
2018-04-17 22:42:25 -0700

    simplyfied pmap

--- src/lightpkg/pmap.nim
+++ src/lightpkg/pmap.nim
@@ -11,38 +11,7 @@ proc worker[T, S](data: seq[T], op: proc (x: T): S {.closure.}): seq[S]{.inline.
   data.map(op)
 
 proc pmap*[T, S](data: seq[T], op: proc (x: T): S {.closure.} ): seq[S]{.inline.} =
-  let
-    num_blocks = num_cpus
-    seg_size = int(data.len / num_blocks)
-
-  var 
-    segments = newSeq[seq[T]](num_blocks)
-    i = 0
-    start_idx = 0
-    end_idx = seg_size
-
-  while end_idx < data.len:
-      segments[i] = data[start_idx..<end_idx]
-      inc(i)
-      start_idx = end_idx
-      end_idx = end_idx + seg_size
-
-  if start_idx < data.len:
-    segments[i] = data[start_idx..<data.len]
+  let segments = data.distribute(num_cpus)
 
   segments.map(proc (segment: seq[T]): auto =
     spawn worker(segment, op)).mapIt(^it).concat
-  
-  #data.map(op)
-
-  #data.mapIt(FlowVar[S], spawn op(it)).mapIt(^it)
-  #  newSeq(result, data.len)
-  #  var vals = newSeq[FlowVar[S]](data.len)
-
-  #  for i in 0..data.high:
-  #      vals[i] = spawn op(data[i])
-  #  sync()
-
-  #  for i in 0..data.high:
-  #      var res = ^vals[i]
-  #      result[i] = res
