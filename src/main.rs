1c094e930fc0920e9649108bedeebe7ea1d6a3dc
Enrique <niofis@gmail.com>
2019-09-16 22:31:34 -0700

    implemented some operators for vector and testing new render loop

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,68 @@
 extern crate sdl2;
 extern crate time;
+use rayon::prelude::*;
 use sdl2::event::Event;
 use sdl2::gfx::primitives::DrawRenderer;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
 use std::error::Error;
+use std::ops;
 
 const WIDTH: usize = 800;
 const HEIGHT: usize = 450;
 
-struct ColorF(f32, f32, f32);
+struct Color(f32, f32, f32);
+struct Vector(f32, f32, f32);
+struct Ray(Vector, Vector);
+struct Camera {
+    eye: Vector,
+    left_top: Vector,
+    left_bottom: Vector,
+    right_top: Vector,
+    width: f32,
+    height: f32,
+}
+enum Primitive {
+    Sphere(Vector, f32),
+    Triangle(Vector, Vector, Vector, Vector),
+}
+
+impl ops::Add<Vector> for Vector {
+    type Output = Vector;
+
+    fn add(self, rhs: Vector) -> Vector {
+        let Vector(x0, y0, z0) = self;
+        let Vector(x1, y1, z1) = rhs;
+        Vector(x0 + x1, y0 + y1, z0 + z1)
+    }
+}
+
+impl ops::Mul<f32> for Vector {
+    type Output = Vector;
+
+    fn mul(self, rhs: f32) -> Vector {
+        let Vector(x, y, z) = self;
+        Vector(x * rhs, y * rhs, z * rhs)
+    }
+}
+
+impl Camera {
+    fn new(eye: Vector, left_top: Vector, width: f32, height: f32) -> Camera {
+        let Vector(x0, y0, z0) = left_top;
+        let left_bottom = Vector(x0, y0 - height, z0);
+        let right_top = Vector(x0 + width, y0, z0);
+
+        Camera {
+            eye,
+            left_top,
+            left_bottom,
+            right_top,
+            width,
+            height,
+        }
+    }
+}
 
 /*fn render() -> Vec<u8> {
     let bpp = 4;
@@ -26,8 +77,8 @@ struct ColorF(f32, f32, f32);
     buffer
 }
 */
-
-fn render() -> Vec<u8> {
+/*
+fn render2() -> Vec<u8> {
     let bpp = 4;
     let pixels = (0..HEIGHT * WIDTH).map(|pixel| {
         //let x = pixel % WIDTH;
@@ -48,7 +99,7 @@ fn render() -> Vec<u8> {
     buffer
 }
 
-fn render2() -> Vec<u8> {
+fn render3() -> Vec<u8> {
     let bpp = 4;
     let pixels: Vec<ColorF> = (0..HEIGHT * WIDTH)
         .map(|pixel| {
@@ -71,6 +122,24 @@ fn render2() -> Vec<u8> {
         buffer[offset + 3] = 255;
     }
 
+    buffer
+}
+*/
+
+fn render() -> Vec<u8> {
+    let bpp = 4;
+    let pixels = (0..HEIGHT * WIDTH).map(|pixel| Color(1.0, 0.0, 0.0));
+
+    let mut buffer: Vec<u8> = vec![0; bpp * WIDTH * HEIGHT];
+    let mut offset = 0;
+    for pixel in pixels {
+        let Color(r, g, b) = pixel;
+        buffer[offset] = (b * 255.99) as u8;
+        buffer[offset + 1] = (g * 255.99) as u8;
+        buffer[offset + 2] = (r * 255.99) as u8;
+        buffer[offset + 3] = 255;
+        offset = offset + 4;
+    }
     buffer
 }
 
@@ -143,7 +212,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         curr_time = time::precise_time_s();
         fps = format!("{:.*}", 2, 1.0 / (curr_time - prev_time));
         prev_time = curr_time;
-        canvas.string(0, 0, &fps, Color::RGB(255, 255, 255))?;
+        canvas.string(0, 0, &fps, sdl2::pixels::Color::RGB(255, 255, 255))?;
         canvas.present();
     }
     Ok(())
